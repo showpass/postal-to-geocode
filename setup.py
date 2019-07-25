e5faf3304b4c11314843a7e4e14f9855ebf6f4cf
Peter Panaguiton <peter@showpass.com>
2019-07-25 16:35:44 -0600

    fix | removed dev hook

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
-import atexit
 from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 
-class PostInstallCommand(install):
-    def run(self):
-        def _post_install():
-            from postal_to_geocode import uncompress_db
-            print('uncompressing db file')
-            uncompress_db()
+def _post_install(setup):
+    def _unzip():
+        from postal_to_geocode import uncompress_db
+        print('uncompressing db file')
+        uncompress_db()
+    _unzip()
+    return setup
 
-        atexit.register(_post_install)
-        install.run(self)
-
-
-setup(
-    name='Postal To Geocode',
-    version='0.01b',
-    packages=['postal_to_geocode',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-    include_package_data=True,
-    zip_safe=False,
-    cmdclass={
-        'install': PostInstallCommand,
-    },
-)
+_post_install(
+    setup(
+        name='Postal To Geocode',
+        version='0.01b',
+        packages=['postal_to_geocode',],
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        long_description=open('README.txt').read(),
+        include_package_data=True,
+        zip_safe=False,
+    ))
